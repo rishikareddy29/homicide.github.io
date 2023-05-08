2b977ccbbd34c9f66558196d1f1e60911b51b9dd
rishikareddy29 <132909172+rishikareddy29@users.noreply.github.com>
2023-05-08 23:44:49 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -3,17 +3,44 @@ $Username = $_POST['uname'];
 $password = $_POST['psw'];
 
 //Database connection
-$conn = new mysqli('localhost', 'root', 'test');
-if($conn->connect_error){
-  die('Connection Failed : '.$conn->connect_error);
-}else{
-  $stmt = $conn->prepare("insert into Userdata(Username, password) values(?, ?)");
-  $stmt->bind-param("ss", $Username, $password);
-  $stmt->execute();
+if (!empty($Username) || !empty($password)){
+  $host = "localhost";
+  $dbUsername = "root";
+  $dbPassword = "";
+  $dbname = "homicide_userdata";
+  //create connection
+  $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
 
-  $stmt->close();
-  $conn->close();
+  if (mysqli_connect_error()){
+    die('Connect Error('. mysqli_connect_error(),')'. mysqli_connect_error());
+  } else {
+    $SELECT = "SELECT Username From Userdata Where Username = ? Limit 1";
+    $INSERT = "INSERT Into Userdata (Username, password) values(?, ?)";
+
+
+    //Prepare statement
+    $stmt = $conn->prepare($SELECT);
+    $stmt->bind_param("s", $Username);
+    $stmt->execute();
+    $stmt->bind_result($Username);
+    $stmt->store_result();
+    $rnum = $stmt->num_rows;
+
+    if ($rnum==0){
+      $stmt->close();
+
+      $stmt = $conn->prepare($INSERT);
+      $stmt->bind_param("ss", $Username, $password);
+      $stmt->execute();
+
+    } 
+    $stmt->close();
+    $conn->close();
+  }
+} else {
+  echo "All fields are required";
+  die();
 }
 
-
  ?>
+
